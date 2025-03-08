b04da3cfb7d90160d02c7822de9c37e82af35928
manju-app-developer <sciencephysics661@gmail.com>
2025-03-08 12:58:38 +0530

    Create Signup.js

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { signup } from "../api";
+
+function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = async () => {
+    const data = await signup({ username, email, password });
+    alert(data.message);
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleSignup}>Signup</button>
+    </div>
+  );
+}
+
+export default Signup;
